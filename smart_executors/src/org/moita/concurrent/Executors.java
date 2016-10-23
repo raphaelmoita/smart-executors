921ca45f2d4bb4ebc55936ea862ad5e90a0ef769
Raphael Moita <raphael.moita@gmail.com>
2016-10-23 20:51:00 -0200

    sharing

---
+++ smart_executors/src/org/moita/concurrent/Executors.java
@@ -0,0 +1,44 @@
+package org.moita.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.function.IntSupplier;
+
+import org.moita.concurrent.interna.SmartExecutor;
+
+/**
+ * @author Raphael Moita.
+ * @email raphael.moita@gmail.com
+ */
+public final class Executors {
+    
+	/**
+	 * Creates a thread pool that dynamically changes the number of parallel threads operating 
+	 * based on rules defined by a supplier. Such supplier is called in a predefined interval time. 
+	 * 
+	 * @param supplierSizeProvider - return the pool size based by any internal rule.
+	 * @param supplierCheckInterval - time interval (in seconds) to reevaluate the thread pool size.
+	 * @return ExecutorService
+	 * 
+	 * @Exemplo: Dynamic thread pool that limits in 100 threads when it is before moon, otherwise 
+	 *           it limits in only 10 threads.
+     * <pre>
+     * ExecutorService pool = newDinamicThreadPool(() -> getMaxThreads(), 2);
+     * 
+     * int getMaxThreads() {
+     *		int maxThreads = 0;
+     *		final Calendar MOON = Calendar.getInstance();
+     *		MOON.set(Calendar.HOUR_OF_DAY, 12);
+     *		if (Calendar.getInstance().before(MOON)) {
+     * 			maxThreads = 100;
+     *		} else {
+     *			maxThreads = 10;
+     *		}
+     *		return maxThreads;    	
+     *	}
+     * </pre>
+     * 
+     */
+    public static ExecutorService newDinamicThreadPool(IntSupplier supplierSizeProvider, int supplierCheckInterval) {
+        return SmartExecutor.create(supplierSizeProvider, supplierCheckInterval);    
+    }
+}
