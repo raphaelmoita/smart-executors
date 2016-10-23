f089d37cb03d7920b1419cfd6845af9b5fedbc3d
Raphael Moita <raphael.moita@gmail.com>
2016-10-23 21:07:11 -0200

    format messages

--- smart_executors/src/org/moita/concurrent/Executors.java
+++ smart_executors/src/org/moita/concurrent/Executors.java
@@ -3,42 +3,47 @@ package org.moita.concurrent;
 import java.util.concurrent.ExecutorService;
 import java.util.function.IntSupplier;
 
-import org.moita.concurrent.interna.SmartExecutor;
+import org.moita.concurrent.intern.SmartExecutor;
 
 /**
  * @author Raphael Moita.
  * @email raphael.moita@gmail.com
  */
 public final class Executors {
-    
+
 	/**
-	 * Creates a thread pool that dynamically changes the number of parallel threads operating 
-	 * based on rules defined by a supplier. Such supplier is called in a predefined interval time. 
+	 * Creates a thread pool that dynamically changes the number of parallel
+	 * threads operating based on rules defined by a supplier. Such supplier is
+	 * called in a predefined time interval.
 	 * 
-	 * @param supplierSizeProvider - return the pool size based by any internal rule.
-	 * @param supplierCheckInterval - time interval (in seconds) to reevaluate the thread pool size.
+	 * @param supplierSizeProvider
+	 *            - return the pool size based by any internal rule.
+	 * @param supplierCheckInterval
+	 *            - time interval (in seconds) to reevaluate the thread pool
+	 *            size.
 	 * @return ExecutorService
 	 * 
-	 * @Exemplo: Dynamic thread pool that limits in 100 threads when it is before moon, otherwise 
-	 *           it limits in only 10 threads.
-     * <pre>
-     * ExecutorService pool = newDinamicThreadPool(() -> getMaxThreads(), 2);
-     * 
-     * int getMaxThreads() {
-     *		int maxThreads = 0;
-     *		final Calendar MOON = Calendar.getInstance();
-     *		MOON.set(Calendar.HOUR_OF_DAY, 12);
-     *		if (Calendar.getInstance().before(MOON)) {
-     * 			maxThreads = 100;
-     *		} else {
-     *			maxThreads = 10;
-     *		}
-     *		return maxThreads;    	
-     *	}
-     * </pre>
-     * 
-     */
-    public static ExecutorService newDinamicThreadPool(IntSupplier supplierSizeProvider, int supplierCheckInterval) {
-        return SmartExecutor.create(supplierSizeProvider, supplierCheckInterval);    
-    }
+	 * @Exemplo: Dynamic thread pool that limits in 100 threads when it is
+	 *           before moon, otherwise it limits in only 10 threads.
+	 * 
+	 *           <pre>
+	 *           ExecutorService pool = newDinamicThreadPool(() -> getMaxThreads(), 2);
+	 * 
+	 *           int getMaxThreads() {
+	 *           	int maxThreads = 0;
+	 *           	final Calendar MOON = Calendar.getInstance();
+	 *           	MOON.set(Calendar.HOUR_OF_DAY, 12);
+	 *           	if (Calendar.getInstance().before(MOON)) {
+	 *           		maxThreads = 100;
+	 *           	} else {
+	 *           		maxThreads = 10;
+	 *           	}
+	 *           	return maxThreads;
+	 *           }
+	 *           </pre>
+	 * 
+	 */
+	public static ExecutorService newDinamicThreadPool(IntSupplier supplierSizeProvider, int supplierCheckInterval) {
+		return SmartExecutor.create(supplierSizeProvider, supplierCheckInterval);
+	}
 }
---
+++ smart_executors/src/org/moita/concurrent/intern/SmartExecutor.java
@@ -0,0 +1,86 @@
+package org.moita.concurrent.intern;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.function.IntSupplier;
+
+public final class SmartExecutor extends ThreadPoolExecutor {
+    
+    private static final int DEFAULT_CHECK_INTERVAL = 5;
+    
+    private final ScheduledExecutorService monitorRegrasScheduler = Executors.newSingleThreadScheduledExecutor();
+    
+    private boolean updatePoolSize = false;
+    
+    private int poolSize = 0;
+    
+    private IntSupplier supplierSizeProvider = null;
+
+    private SmartExecutor(IntSupplier supplierSizeProvider) {        
+        this(supplierSizeProvider, DEFAULT_CHECK_INTERVAL);
+    }
+    
+    private SmartExecutor(IntSupplier supplierSizeProvider, int supplierCheckInterval) {
+        super(supplierSizeProvider.getAsInt(), supplierSizeProvider.getAsInt(), 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        this.supplierSizeProvider = supplierSizeProvider;
+        this.monitorRegrasScheduler.scheduleAtFixedRate(new PoolManager(this), 0L, supplierCheckInterval, TimeUnit.SECONDS);
+    }
+
+    public static SmartExecutor create(IntSupplier pMetodo) {
+        return new SmartExecutor(pMetodo);
+    }
+    
+    public static SmartExecutor create(IntSupplier pMetodo, int pIntervaloVerificacao) {
+        return new SmartExecutor(pMetodo, pIntervaloVerificacao);
+    }
+    
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        if (this.updatePoolSize) {
+            setCorePoolSize(this.poolSize);
+            setMaximumPoolSize(this.poolSize);
+            this.updatePoolSize = false;
+        }
+        super.beforeExecute(t, r);
+    }
+
+    private synchronized void setPoolSize(int pPoolSize) {
+        this.poolSize = pPoolSize;
+    }
+    
+    private synchronized void setUpdatePoolSize(boolean pUpdatePoolSize) {
+        this.updatePoolSize = pUpdatePoolSize;
+    }
+    
+    private int getNroMaxThreads() {
+        return this.supplierSizeProvider.getAsInt();
+    }
+    
+    /**
+     * 
+     */
+    class PoolManager implements Runnable {
+        
+    	private SmartExecutor pool = null;
+        
+        private int currentPoolSize = 0;
+
+        public PoolManager(SmartExecutor pPool) {
+            this.pool = pPool;
+        }
+
+        @Override
+        public void run() {   
+            int lPoolSize = getNroMaxThreads();
+            if (this.currentPoolSize != lPoolSize) {   
+                this.pool.setUpdatePoolSize(true);
+                this.pool.setPoolSize(lPoolSize); 
+                this.currentPoolSize = lPoolSize;
+            } 
+        }        
+    }
+
+}
